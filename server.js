01a20c07236f061f79cd4d80190d8a0f76373379
Aditi Hotti <155968055+aditisha019@users.noreply.github.com>
2025-06-23 01:35:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 require('dotenv').config()
 const app = require('./src/app');
 
+app.get('/', (req, res) => {
+  res.send('✅ Backend is live!');
+});
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000')
 })
